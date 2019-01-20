c78fb963499bd26cf4703d25f8bffa69e39a47ca
suren-vanyan <vanyansuren@gmail.com>
2019-01-21 02:16:30 +0400

    add dsecription for algoritm

--- Sorting Algorithms/SelectSortImplement/SelectSortImplement/Program.cs
+++ Sorting Algorithms/SelectSortImplement/SelectSortImplement/Program.cs
@@ -8,12 +8,15 @@ namespace SelectSortImplement
 {
     class Program
     {
-        //public static void Swap(int[] array,int left,int right)
-        //{
-        //    int temp = array[left];
-        //    array[left] = array[right];
-        //    array[right] = temp;
-        //}
+        //The selection sort algorithm sorts an array by repeatedly finding the minimum element
+        //(considering ascending order) from unsorted part and putting it at the beginning.
+        //The algorithm maintains two subarrays in a given array.
+
+        //1) The subarray which is already sorted.
+        //2) Remaining subarray which is unsorted.
+
+        //In every iteration of selection sort, the minimum element(considering ascending order)
+        //from the unsorted subarray is picked and moved to the sorted subarray.
 
         public static void Swap(ref int first, ref int second)
         {
