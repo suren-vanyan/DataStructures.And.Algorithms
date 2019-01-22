ab371ef6f30bb79ddcaaf75f90178ad25ca98a7b
suren-vanyan <vanyansuren@gmail.com>
2019-01-22 15:08:13 +0400

    add Binary tree

---
+++ Three/Binary_Tree/Binary_Tree/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTreeNode<int> treeNode1 = new BinaryTreeNode<int>(6);
+            BinaryTreeNode<int> treeNode2 = new BinaryTreeNode<int>(8);
+            BinaryTreeNode<int> treeNode3 = new BinaryTreeNode<int>(4);
+
+            if (treeNode1.ComareNode(treeNode2) >= 0) { treeNode1.Left = treeNode2; }  
+            else { treeNode1.Right = treeNode2; }                                      
+                                                                                        
+            if (treeNode1.ComareNode(treeNode3) >= 0) { treeNode1.Left = treeNode3; }                                                                  // 4    8
+            else { treeNode1.Right = treeNode3; }
+
+            Console.WriteLine($"  {treeNode1.Value}");
+            Console.Write(treeNode3.Value);
+            Console.WriteLine($"   {treeNode2.Value}");
+        }
+    }
+}
